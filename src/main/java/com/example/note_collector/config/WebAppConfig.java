1e4a1ad7d867e4840aabc0e56c226f928fde2310
sangeeth <ssulochanaherath@gmail.com>
2024-09-29 23:38:05 +0530

    Added the maven dependencies.

--- src/main/java/com/example/note_collector/config/WebAppConfig.java
+++ src/main/java/com/example/note_collector/config/WebAppConfig.java
@@ -1,5 +1,11 @@
 package com.example.note_collector.config;
 
+import jakarta.servlet.annotation.MultipartConfig;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MultipartConfig
 public class WebAppConfig {
     public ModelMapper modelMapper() {
         return new ModelMapper();
--- src/main/java/com/example/note_collector/config/WebAppRootConfig.java
+++ src/main/java/com/example/note_collector/config/WebAppRootConfig.java
@@ -1,4 +1,9 @@
 package com.example.note_collector.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
 public class WebAppRootConfig {
 }
