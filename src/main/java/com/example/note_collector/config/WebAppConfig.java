2d604da1daa8bb8709d0b671e4618c72a11f5d07
sangeeth <ssulochanaherath@gmail.com>
2024-09-29 23:16:39 +0530

    Added the maven dependencies.

---
+++ src/main/java/com/example/note_collector/config/WebAppConfig.java
@@ -0,0 +1,7 @@
+package com.example.note_collector.config;
+
+public class WebAppConfig {
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
---
+++ src/main/java/com/example/note_collector/config/WebAppRootConfig.java
@@ -0,0 +1,4 @@
+package com.example.note_collector.config;
+
+public class WebAppRootConfig {
+}
