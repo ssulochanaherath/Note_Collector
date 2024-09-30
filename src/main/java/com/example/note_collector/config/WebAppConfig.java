532b739668c401b40dd8ec54b12be243cf484205
sangeeth <ssulochanaherath@gmail.com>
2024-09-30 22:06:21 +0530

    Created web config.

--- src/main/java/com/example/note_collector/config/WebAppConfig.java
+++ src/main/java/com/example/note_collector/config/WebAppConfig.java
@@ -2,9 +2,13 @@ package com.example.note_collector.config;
 
 import jakarta.servlet.annotation.MultipartConfig;
 import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@ComponentScan(basePackages = "com.example.note_collector")
+@EnableWebMvc
 @MultipartConfig
 public class WebAppConfig {
     public ModelMapper modelMapper() {
